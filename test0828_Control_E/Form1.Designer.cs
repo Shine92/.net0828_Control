9bbe775213f205ec2f74ccad1d4b1b2954859c38
shaoming Huang <msn8971@gmail.com>
2017-08-28 17:17:20 +0800

    C#.NET Contorl
    Button
    Button_Click Event
    TextBox

---
+++ test0828_Control_E/Form1.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace test0828_Control_E
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserName = new System.Windows.Forms.TextBox();
+            this.Password = new System.Windows.Forms.TextBox();
+            this.ok = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Notes = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.PwdShow = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // UserName
+            // 
+            this.UserName.Location = new System.Drawing.Point(22, 66);
+            this.UserName.Name = "UserName";
+            this.UserName.Size = new System.Drawing.Size(160, 22);
+            this.UserName.TabIndex = 1;
+            // 
+            // Password
+            // 
+            this.Password.Location = new System.Drawing.Point(23, 115);
+            this.Password.Name = "Password";
+            this.Password.Size = new System.Drawing.Size(159, 22);
+            this.Password.TabIndex = 3;
+            this.Password.UseSystemPasswordChar = true;
+            // 
+            // ok
+            // 
+            this.ok.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.ok.Location = new System.Drawing.Point(225, 66);
+            this.ok.Name = "ok";
+            this.ok.Size = new System.Drawing.Size(93, 71);
+            this.ok.TabIndex = 6;
+            this.ok.Text = "OK";
+            this.ok.UseVisualStyleBackColor = true;
+            this.ok.Click += new System.EventHandler(this.ok_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.ForeColor = System.Drawing.Color.Red;
+            this.label1.Location = new System.Drawing.Point(18, 42);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(105, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "&User Name:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label2.ForeColor = System.Drawing.Color.Red;
+            this.label2.Location = new System.Drawing.Point(19, 91);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(92, 21);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "&Password:";
+            // 
+            // Notes
+            // 
+            this.Notes.Location = new System.Drawing.Point(23, 168);
+            this.Notes.Multiline = true;
+            this.Notes.Name = "Notes";
+            this.Notes.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.Notes.Size = new System.Drawing.Size(159, 81);
+            this.Notes.TabIndex = 5;
+            this.Notes.WordWrap = false;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label3.Location = new System.Drawing.Point(23, 144);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 21);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Notes:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("新細明體", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label4.Location = new System.Drawing.Point(23, 256);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(142, 21);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Password Show:";
+            // 
+            // PwdShow
+            // 
+            this.PwdShow.Location = new System.Drawing.Point(27, 281);
+            this.PwdShow.Name = "PwdShow";
+            this.PwdShow.Size = new System.Drawing.Size(155, 22);
+            this.PwdShow.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(371, 370);
+            this.Controls.Add(this.PwdShow);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.Notes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.ok);
+            this.Controls.Add(this.Password);
+            this.Controls.Add(this.UserName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserName;
+        private System.Windows.Forms.TextBox Password;
+        private System.Windows.Forms.Button ok;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Notes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox PwdShow;
+    }
+}
+
